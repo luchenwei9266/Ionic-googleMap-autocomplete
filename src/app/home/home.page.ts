10c72999fd5cb5d2cbee5cb7141357d5b616a166
luchenwei9266 <a413423382@163.com>
2019-03-27 14:21:05 +0800

    done

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+/// <reference types="@types/googlemaps" />
+import { Component, ViewChild, OnInit, AfterViewInit , NgZone } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit, AfterViewInit {
+	@ViewChild('addresstext') addresstext: any;
 
+	public adressType: string;
+	public autocompleteInput: string;
+
+	constructor(
+		private _ngZone: NgZone
+	) {}
+
+	ngOnInit() {
+	}
+
+	ngAfterViewInit() {
+		setTimeout(() => {
+			this.getPlaceAutocomplete();
+		},1000);
+	}
+
+	private getPlaceAutocomplete() {
+			let ele = document.getElementById('addresstext').getElementsByTagName('input')[0];
+			// let ele = this.addresstext.nativeElement;
+			const autocomplete = new google.maps.places.Autocomplete(ele,
+				{
+					// googlemap set country
+						componentRestrictions: { country: 'CN' },
+						types: [this.adressType]  // 'establishment' / 'address' / 'geocode'
+				});
+			google.maps.event.addListener(autocomplete, 'place_changed', () => {
+				const place = autocomplete.getPlace();
+				this.autocompleteInput = place.formatted_address;
+				this._ngZone.run(()=>{});
+			});
+	}
 }
